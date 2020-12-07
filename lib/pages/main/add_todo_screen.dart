6db6eed9c8bd12cadb72dbdcddc37810d1698deb
ycrao573 <raoyuchenom@gmail.com>
2020-12-07 21:08:54 +0800

    first implement

--- lib/pages/main/add_todo_screen.dart
+++ lib/pages/main/add_todo_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:io' as io;
+
 import 'package:flutter/material.dart';
+
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter/services.dart';
 import 'package:gogogoals/components/todo_badge.dart';
 import 'package:gogogoals/model/hero_id_model.dart';
@@ -213,6 +218,39 @@ class _AddTodoScreenState extends State<AddTodoScreen> {
           return Container(
             color: Colors.white,
           );
+        }
+
+        io.File pickedImage;
+        var text = '';
+
+        bool imageLoaded = false;
+
+        Future pickImage() async {
+          var awaitImage =
+              await ImagePicker.pickImage(source: ImageSource.gallery);
+
+          setState(() {
+            pickedImage = awaitImage;
+            imageLoaded = true;
+          });
+          FirebaseVisionImage visionImage =
+              FirebaseVisionImage.fromFile(pickedImage);
+          TextRecognizer textRecognizer =
+              FirebaseVision.instance.textRecognizer();
+          VisionText visionText =
+              await textRecognizer.processImage(visionImage);
+
+          for (TextBlock block in visionText.blocks) {
+            for (TextLine line in block.lines) {
+              for (TextElement word in line.elements) {
+                setState(() {
+                  text = text + word.text + ' ';
+                });
+              }
+              text = text + '\n';
+            }
+          }
+          textRecognizer.close();
         }
 
         var _task = model.tasks.firstWhere((it) => it.id == widget.taskId);
@@ -564,7 +602,57 @@ class _AddTodoScreenState extends State<AddTodoScreen> {
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.all(Radius.circular(10)),
                   ),
-                )
+                ),
+                Column(
+                  children: <Widget>[
+                    SizedBox(height: 100.0),
+                    imageLoaded
+                        ? Center(
+                            child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              boxShadow: const [
+                                BoxShadow(blurRadius: 20),
+                              ],
+                            ),
+                            margin: EdgeInsets.fromLTRB(0, 0, 0, 8),
+                            height: 250,
+                            child: Image.file(
+                              pickedImage,
+                              fit: BoxFit.cover,
+                            ),
+                          ))
+                        : Container(),
+                    SizedBox(height: 10.0),
+                    Center(
+                      child: FlatButton.icon(
+                        icon: Icon(
+                          Icons.photo_camera,
+                          size: 100,
+                        ),
+                        label: Text(''),
+                        textColor: Theme.of(context).primaryColor,
+                        onPressed: () async {
+                          pickImage();
+                        },
+                      ),
+                    ),
+                    SizedBox(height: 10.0),
+                    SizedBox(height: 10.0),
+                    text == ''
+                        ? Text('Text will display here')
+                        : Expanded(
+                            child: SingleChildScrollView(
+                              child: Padding(
+                                padding: const EdgeInsets.all(15.0),
+                                child: Text(
+                                  text,
+                                ),
+                              ),
+                            ),
+                          ),
+                  ],
+                ),
               ],
             ),
           ),
